73ac39a42bf32e10a015f7896f84fcd53fb28af8
unknown <Bitsa033>
2026-08-20 19:28:46 +0100

    nouveau commit 2

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.use(express.static(__dirname));
+
 // Configuration Gmail
 const transporter = nodemailer.createTransport({
     service: 'gmail',
